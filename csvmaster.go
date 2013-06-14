3589c292756e0e9d1021adcad32158348f3c0d97
spiffytech <spiffytech@gmail.com>
2013-06-14 12:09:49 -0400

    Added ability to use short flags. Yay short flags! Happy Flag Day, America!

--- csvmaster.go
+++ csvmaster.go
@@ -15,15 +15,60 @@ const nilCommentRune = "TOTALLYNOTACOMMENTCHAR"
 
 // TODO: Add support for specifying fields by field header name instead of just number
 // TODO: check ReadRuneFromString instead of existing technique
+// TODO: Print version
+// TODO: Publish binaries
+// TODO: Include test suite
+// TODO: Accept filename without --filename flag
 
-var inSep = flag.String("in-sep", ",", "Single character field separator used by your input")
-var outSep = flag.String("out-sep", ",", "Single-character field separator to use when printing multiple columns in your output. Only valid if outputting something meant to be passed to cut/awk, and not a properly-formatted, quoted CSV file.")
-var commentRune = flag.String("comment-char", nilCommentRune, "Single-character field separator to use when printing multiple columns in your output. Only valid if outputting something meant to be passed to cut/awk, and not a properly-formatted, quoted CSV file.")
+var inSep string
+var outSep string
+var commentRune string
 
-var filename = flag.String("file", "", "File to read from. If not specified, program reads from stdin.")
+var filename string
 
-var fieldNumsRaw = flag.String("field-nums", "", "Comma-separated list of field indexes (starting at 0) to print to the command line")
+var fieldNumsRaw string
 var noRFC = flag.Bool("no-rfc", false, "Program defaults to printing RFC 4180-compliant, quoted, well-formatted CSV. If this flag is supplied, output is returned as a string naively joined by --out-sep. --no-rfc is assumed to imply you want to pass the output to naive tools like cut or awk, and in that case, it is recommended that you select an --out-sep that is unlikely to be in youc content, such as a pipe or a backtick.")
+
+func init() {
+    const (
+        filenameFlagLong = "file"
+        filenameFlagShort = "f"
+        filenameDesc = "File to read from. If not specified, program reads from stdin."
+
+        fieldNumsRawFlagLong = "fields"
+        fieldNumsRawFlagShort = "F"
+        fieldNumsRawDesc =  "Comma-separated list of field indexes (starting at 0) to print to the command line"
+
+        inSepFlagLong = "in-sep"
+        inSepFlagShort = "i"
+        inSepDesc = "Single character field separator used by your input"
+        inSepDefault = ","
+
+        outSepFlagLong = "out-sep"
+        outSepFlagShort = "o"
+        outSepDesc = "Single-character field separator to use when printing multiple columns in your output. Only valid if outputting something meant to be passed to cut/awk, and not a properly-formatted, quoted CSV file."
+        outSepDefault = ","
+
+        commentRuneFlagLong = "comment-char"
+        commentRuneFlagShort = "c"
+        commentRuneDesc = "Single-character field separator to use when printing multiple columns in your output. Only valid if outputting something meant to be passed to cut/awk, and not a properly-formatted, quoted CSV file."
+    )
+
+    flag.StringVar(&filename, filenameFlagLong, "", filenameDesc)
+    flag.StringVar(&filename, filenameFlagShort, "", filenameDesc)
+
+    flag.StringVar(&fieldNumsRaw, fieldNumsRawFlagLong, "", fieldNumsRawDesc)
+    flag.StringVar(&fieldNumsRaw, fieldNumsRawFlagShort, "", fieldNumsRawDesc)
+
+    flag.StringVar(&inSep, inSepFlagLong, inSepDefault, inSepDesc)
+    flag.StringVar(&inSep, inSepFlagShort, inSepDefault, inSepDesc)
+
+    flag.StringVar(&outSep, outSepFlagLong, outSepDefault, outSepDesc)
+    flag.StringVar(&outSep, outSepFlagShort, outSepDefault, outSepDesc)
+
+    flag.StringVar(&commentRune, commentRuneFlagLong, nilCommentRune, commentRuneDesc)
+    flag.StringVar(&commentRune, commentRuneFlagShort, nilCommentRune, commentRuneDesc)
+}
 
 func main() {
     flag.Parse()
@@ -31,9 +76,9 @@ func main() {
     var fieldNums []int
 
     // Identify the numbers you want to print out
-    *fieldNumsRaw = strings.Trim(*fieldNumsRaw, ",")
-    if *fieldNumsRaw != "" {
-        for _, numStr := range strings.Split(*fieldNumsRaw, ",") {
+    fieldNumsRaw = strings.Trim(fieldNumsRaw, ",")
+    if fieldNumsRaw != "" {
+        for _, numStr := range strings.Split(fieldNumsRaw, ",") {
             numStr := strings.TrimSpace(numStr)
             numInt, err := strconv.Atoi(numStr)
             if err != nil {
@@ -44,10 +89,10 @@ func main() {
     }
 
     var reader *bufio.Reader
-    if *filename == "" {
+    if filename == "" {
         reader = bufio.NewReader(os.Stdin)
     } else {
-        f, err := os.Open(*filename)
+        f, err := os.Open(filename)
         if err != nil {
             panic(err)
         }
@@ -58,14 +103,14 @@ func main() {
     csvReader := csv.NewReader(reader)
     csvReader.LazyQuotes = true
     csvReader.TrailingComma = true
-    csvReader.Comma = getSeparator(*inSep)
+    csvReader.Comma = getSeparator(inSep)
     csvReader.FieldsPerRecord = -1
-    if *commentRune != nilCommentRune {
-        csvReader.Comment = ([]rune(*commentRune))[0]
+    if commentRune != nilCommentRune {
+        csvReader.Comment = ([]rune(commentRune))[0]
     }
 
     csvWriter := csv.NewWriter(os.Stdout)
-    csvWriter.Comma = getSeparator(*outSep)
+    csvWriter.Comma = getSeparator(outSep)
 
     for {
         fields, err := csvReader.Read()
@@ -77,7 +122,7 @@ func main() {
         }
 
         var toPrint []string
-        if *fieldNumsRaw == "" {  // Print all fields
+        if fieldNumsRaw == "" {  // Print all fields
             toPrint = fields
         } else {
             for _, num := range fieldNums {
@@ -92,7 +137,7 @@ func main() {
         if *noRFC == false {
             csvWriter.Write(toPrint)
         } else {
-            fmt.Println(strings.Join(toPrint, *outSep))
+            fmt.Println(strings.Join(toPrint, outSep))
         }
     }
     if *noRFC == false {
